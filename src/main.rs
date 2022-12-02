b0d85c56a0c88673a21cbc18eaec77676cd599cc
Adam Lefevre <adamtlefevre@gmail.com>
2022-12-02 13:50:49 -0600

    simplify

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,10 @@ fn problem1(file_name: &str) {
 
     let elves: Vec<&str> = data.trim().split("\n\n").collect();
     
-    let mut max: u32 = 0;
     let mut top_three: Vec<u32> = vec![0,0,0];
     for elf in elves {
-        let total: u32 = elf.split('\n').map(|amount| amount.parse::<u32>().unwrap()).sum();
-        if total > max {
-            max = total
-        }
+        let mut candidate: u32 = elf.split('\n').map(|amount| amount.parse::<u32>().unwrap()).sum();
         
-        let mut candidate = total;
         for slot in top_three.iter_mut() {
             if candidate > *slot {
                 candidate = replace(slot, candidate);
@@ -39,7 +34,7 @@ fn problem1(file_name: &str) {
         }
     }
 
-    println!("Largest total is: {}", max);
+    println!("Largest total is: {}", top_three[0]);
     println!("Largest 3 totals summed are: {}", top_three.iter().sum::<u32>());
 }
 
